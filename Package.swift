53c5d5aceca48bfc85792de552180092fc7e2e62
JasonZhou <jasonz1360@gmail.com>
2025-07-14 22:56:35 +0800

    Remove binary targets for onnxruntime and webrtc_audio_processing from Package.swift

--- Package.swift
+++ Package.swift
@@ -33,16 +33,6 @@ let package = Package(
             name: "RealTimeCutVADCXXLibrary",
             url: "https://github.com/helloooideeeeea/RealTimeCutVADLibraryForXCFramework/releases/download/v1.0.3/RealTimeCutVADCXXLibrary.xcframework.zip",
             checksum: "00c4a8cd5a22e634f34d744eb06aa88a4494e12a5f9d4573989129a1f4c7e76d"
-        ),
-        .binaryTarget(
-            name: "onnxruntime",
-            url: "https://github.com/helloooideeeeea/RealTimeCutVADLibraryForXCFramework/releases/download/v1.0.6/onnxruntime.xcframework.zip",
-            checksum: "ce9fafa489cc527c2c9a1297ece8a7bb9c024ec3d51da0b3d08bd5007583110b"
-        ),
-        .binaryTarget(
-            name: "webrtc_audio_processing",
-            url: "https://github.com/helloooideeeeea/RealTimeCutVADLibraryForXCFramework/releases/download/v1.0.6/webrtc_audio_processing.xcframework.zip",
-            checksum: "5253984bdf20fb483b34c884f71a32b1f9a3078a932c601fed72bb4ac1565e86"
         )
     ]
 )
